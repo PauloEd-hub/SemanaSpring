da7ef567cd5300ef59de75a51a86dd62eb1cf142
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-11-15 01:28:01 -0300

    Project created.

---
+++ backenddsmetas/src/test/java/com/devsuperior/backenddsmetas/BackenddsmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.backenddsmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenddsmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
