a3b179d3b0483a40503bb638362114293025dd58
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-11-18 02:44:50 -0300

    Twilio sms.

--- backenddsmetas/src/main/java/com/devsuperior/backenddsmetas/controller/SaleController.java
+++ backenddsmetas/src/main/java/com/devsuperior/backenddsmetas/controller/SaleController.java
@@ -2,15 +2,17 @@ package com.devsuperior.backenddsmetas.controller;
 
 import com.devsuperior.backenddsmetas.entities.Sale;
 import com.devsuperior.backenddsmetas.service.SaleService;
+import com.devsuperior.backenddsmetas.service.SmsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,6 +20,9 @@ public class SaleController {
 
     @Autowired
     private SaleService saleService;
+    
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -26,5 +31,12 @@ public class SaleController {
             Pageable pageable) {
         return saleService.findSales( minDate, maxDate,pageable);
 
+    }
+    
+    
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+    	smsService.sendSms(id);
+    	
     }
 }
